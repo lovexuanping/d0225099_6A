f1a8a8d8e7dfe36643b709441b41072114305378
lovexuanping <guanping91@gmail.com>
2015-12-21 16:40:20 +0800

    next update

--- src/useX/q6a.java
+++ src/useX/q6a.java
@@ -1,10 +1,35 @@
 package useX;
 
+import java.util.*;
+
+interface Component {
+    void quetionset();
+}
+class frame implements componentx {
+    private String quetion;
+    frame(String quetion) {
+        this.quetion = quetion;
+    }
+    public void quetionset() {
+        System.out.println(quetion);
+    }
+}
+class quetion implements componentx {
+    List<componentx> list = new ArrayList<componentx>();
+    public void add(componentx component) {
+        list.add(component);
+    }
+    public void quetionset() {
+        for(componentx component : list) {
+        	component.quetionset();
+        }
+    }
+}
 public class q6a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
